1b2856798a617c2a892e96fa5240830432922e08
rsballek <ballekr@moravian.edu>
2020-05-05 11:29:44 -0400

    added Test and Test main

---
+++ TestJunit.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestJunit {
+
+    public void fileOf___(){
+
+    }
+}
---
+++ TestMain.java
@@ -0,0 +1,11 @@
+import org.junit.*;
+
+import java.io.File;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestMain {
+    public static  void main(String[] args){
+        TestJunit test = new TestJunit();
+    }
+}
